d5ca3df958afe1ba2b1f889a1cd48410deb3e248
Man Wai Wong <william.manwai@gmail.com>
2019-02-20 23:33:37 +0800

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header";
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby" />
+    <Header headerText="Lun DUi" />
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
+  </div>
+)
